19d99781ec182cbd8ac332e5c88a38341f620990
Egemen Ayhan <eayhan3469@gmail.com>
2023-01-13 13:29:51 +0300

    CubeWallCreator added.

---
+++ Assets/Scripts/CubeWallCreator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeWallCreator : MonoBehaviour
+{
+    [SerializeField] private Transform _startingTransform;
+    [SerializeField] private GameObject _cubeObject;
+
+    [Header("Wall Parameters")]
+    [SerializeField] private int _height;
+    [SerializeField] private int _width;
+    [SerializeField] private float _cubeSize;
+
+    private MaterialPropertyBlock _materialPropertyBlock;
+
+    private void Awake()
+    {
+        _materialPropertyBlock = new MaterialPropertyBlock();
+    }
+
+    void Start()
+    {
+        for (int y = 0; y < _height; y++)
+        {
+            for (int x = 0; x < _width; x++)
+            {
+                var cube = Instantiate(_cubeObject, transform);
+                cube.transform.position = new Vector3(_startingTransform.position.x + (x * _cubeSize), _startingTransform.position.y + (y * _cubeSize), 0);
+                cube.transform.localScale = Vector3.one * _cubeSize;
+                cube.GetComponent<Rigidbody>().isKinematic = true;
+
+                var renderer = cube.GetComponent<Renderer>();
+                renderer.GetPropertyBlock(_materialPropertyBlock);
+                _materialPropertyBlock.SetColor("_Color", Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f)); //TODO: Renk değişimi aynı rengin farklı tonlarında rastgele yapılacak
+                renderer.SetPropertyBlock(_materialPropertyBlock);
+            }
+        }
+    }
+}
